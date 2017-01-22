0215ed530c13f8125a2c50c3b9a4d30803ad77b1
cqx820 <673545775@qq.com>
2017-01-21 18:37:13 -0700

    Exception handle

--- PA1/HttpRequest.java
+++ PA1/HttpRequest.java
@@ -1,29 +1,24 @@
 package ProxyProject;
 
 import java.io.*;
-import java.util.*;
 import java.net.*;
 
 public class HttpRequest {
-
 	private Socket socket = null;
-
 	public HttpRequest(Socket socket) {
-
-		this.socket = socket;
+		this.socket = socket; //Client socket
 	}
 
 	public void parsing() throws IOException {
 		BufferedReader ip = null;
 		DataOutputStream to = null;
 		String inputLine;
-
 		try {
-
 			ip = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			out.println("Something");
-
+			out.println("Please enter your request, \"GET\" request only");
+			String protocol, host, path, query;
+			int port;
 			while ((inputLine = ip.readLine()) != null) {
 				// try {
 				// StringTokenizer tokens = new StringTokenizer(inputLine);
@@ -31,72 +26,62 @@ public class HttpRequest {
 				// } catch (Exception e) {
 				// break;
 				// }
-				
 				if (inputLine == null || inputLine.equals("")) {
-					throw new IOException("Request line is null");
+					throw new IOException("Please enter a conrrect request");
 				}
-
 				String[] tokens = inputLine.split(" ");
 				if (tokens.length < 3) {
-					throw new Exception("Incomplete tokens");
+					throw new Exception("Incomplete request");
 				} else {
 					String method = tokens[0];
 					String uri = tokens[1];
 					String version = tokens[2];
 
 					if (!method.equals("GET")) {
-						throw new Exception("not get");
+						throw new Exception("\"GET\" method only");
 					}
-
 					URL url = new URL(uri);
-					String host = url.getHost();
+					host = url.getHost();
 					if (host == null || host.equals("")) {
-						throw new Exception("host wrong");
+						throw new Exception("Unable to get host info");
 					}
-
-					int port = url.getPort();
-					String protocol = url.getProtocol();
-					String path = url.getPath();
+					port = url.getPort();
+					protocol = url.getProtocol();
+					path = url.getPath();
 					if (path == null || path.equals("")) {
 						path = "/";
 					}
-
 					if (port == -1) {
 						if (protocol.equals("http")) {
-							port = 80;// default
+							port = 80;// default port
 						} else {
-							throw new Exception("wrong portcio");
+							throw new Exception("Wrong protocol, please check");//http procotol only
 						}
 					}
-
-					String query = url.getQuery();
+					query = url.getQuery();
 					if (query != null && !query.equals("")) {
 						path += "?" + query;
 
 					}
-
 					if (!version.equals("HTTP/1.0")) {
 						version = "HTTP/1.0";
+						System.out.println("HTTP version is changed");
 					}
-					Socket newSocket = new Socket(host, port);
+					Socket newSocket = new Socket(host, port);//New socket to connect server
 					to = new DataOutputStream(newSocket.getOutputStream());
 					String toWrite = method + " " + path + " " + version + "\r\n" + "Host: " + host + "\r\n"
 							+ "Connection: close\r\n\r\n";
-					to.writeBytes(toWrite);
-
+					to.writeBytes(toWrite);//Send request to server
 					to.flush();
-
 					BufferedReader rd = new BufferedReader(new InputStreamReader(newSocket.getInputStream()));
 					String line;
 					StringBuilder response = new StringBuilder();
 					response.append("\r\n");
 					while ((line = rd.readLine()) != null) {
-						response.append(line + "\r\n");
-				
+						response.append(line + "\r\n"); //receive and build response string from server
 					}
 					DataOutputStream toClient = new DataOutputStream(socket.getOutputStream());
-					toClient.writeBytes(response.toString());
-				
+					toClient.writeBytes(response.toString()); //Write response to client
 					toClient.flush();
 					toClient.close();
 					to.close();
@@ -104,34 +89,19 @@ public class HttpRequest {
 						if (newSocket != null)
 							newSocket.close();
 					} catch (IOException e) {
-
+						System.err.println("Unable to close socket");
 					}
 				}
-
 			}
-
 		} catch (Exception e) {
-
+			System.err.println(e.getMessage());
 		} finally {
 			try {
 				if (socket != null)
 					socket.close();
 			} catch (IOException e) {
-
+				System.err.println("Unable to close socket");
 			}
-
 		}
 	}
 }
-
-// URL url = new URL("http://www.runoob.com/index.html?language=cn#j2se");
-// System.out.println("URL 为：" + url.toString());
-// System.out.println("协议为：" + url.getProtocol());
-// System.out.println("验证信息：" + url.getAuthority());
-// System.out.println("文件名及请求参数：" + url.getFile());
-// System.out.println("主机名：" + url.getHost());
-// System.out.println("路径：" + url.getPath());
-// System.out.println("端口：" + url.getPort());
-// System.out.println("默认端口：" + url.getDefaultPort());
-// System.out.println("请求参数：" + url.getQuery());
-// System.out.println("定位位置：" + url.getRef());
--- PA1/ProxyServer.java
+++ PA1/ProxyServer.java
@@ -5,6 +5,7 @@ import java.net.*;
 
 public class ProxyServer {
 	public static void main(String[] args) throws IOException {
+
 		ServerSocket serverSocket = null;
 
 		int port = 8970;
@@ -19,14 +20,32 @@ public class ProxyServer {
 
 		try {
 			while (true) {
-				//serverSocket.accept();
+				// serverSocket.accept();
 				new HttpRequest(serverSocket.accept()).parsing();
 			}
 		} catch (Exception ex) {
 			System.err.println(ex.getMessage());
 
 		} finally {
-			serverSocket.close();
+			try {
+				if (serverSocket != null)
+					serverSocket.close();
+			} catch (IOException e) {
+				System.err.println("Unable to close socket");
+			}
 		}
+
+		// URL url = new
+		// URL("http://www.runoob.com/index.html?language=cn#j2se");
+		// System.out.println("URL 为：" + url.toString());
+		// System.out.println("协议为：" + url.getProtocol());
+		// System.out.println("验证信息：" + url.getAuthority());
+		// System.out.println("文件名及请求参数：" + url.getFile());
+		// System.out.println("主机名：" + url.getHost());
+		// System.out.println("路径：" + url.getPath());
+		// System.out.println("端口：" + url.getPort());
+		// System.out.println("默认端口：" + url.getDefaultPort());
+		// System.out.println("请求参数：" + url.getQuery());
+		// System.out.println("定位位置：" + url.getRef());
 	}
 }
