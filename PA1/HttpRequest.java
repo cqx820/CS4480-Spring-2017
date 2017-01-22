9714205dd995462944159002f98fe721e7bcdfa5
cqx820 <673545775@qq.com>
2017-01-21 19:39:21 -0700

    HTTPException with error code

--- PA1/HttpRequest.java
+++ PA1/HttpRequest.java
@@ -2,11 +2,17 @@ package ProxyProject;
 
 import java.io.*;
 import java.net.*;
+import javax.xml.ws.http.HTTPException;
 
 public class HttpRequest {
+	private final int BAD_REQUEST_CODE = 400;
+	private final String BAD_REQUEST_MESSAGE = "Bad Request";
+	private final int NOT_IMPLEMENTED_CODE = 501;
+	private final String NOT_IMPLEMENTED_MESSAGE = "Not Implemented";
 	private Socket socket = null;
+
 	public HttpRequest(Socket socket) {
-		this.socket = socket; //Client socket
+		this.socket = socket; // Client socket
 	}
 
 	public void parsing() throws IOException {
@@ -27,23 +33,39 @@ public class HttpRequest {
 				// break;
 				// }
 				if (inputLine == null || inputLine.equals("")) {
-					throw new IOException("Please enter a conrrect request");
+					System.err.println("Error Code " + BAD_REQUEST_CODE + ": " + BAD_REQUEST_MESSAGE);
+					throw new HTTPException(BAD_REQUEST_CODE);
+					// throw new IOException("Please enter a conrrect request");
 				}
 				String[] tokens = inputLine.split(" ");
 				if (tokens.length < 3) {
-					throw new Exception("Incomplete request");
+					System.err.println("Error Code " + BAD_REQUEST_CODE + ": " + BAD_REQUEST_MESSAGE);
+					throw new HTTPException(BAD_REQUEST_CODE);
+					// throw new Exception("Incomplete request");
 				} else {
 					String method = tokens[0];
 					String uri = tokens[1];
 					String version = tokens[2];
 
 					if (!method.equals("GET")) {
-						throw new Exception("\"GET\" method only");
+						if (method.equals("HEAD") || method.equals("POST") || method.equals("DELETE")
+								|| method.equals("PUT") || method.equals("OPTIONS") || method.equals("TRACE")
+								|| method.equals("CONNECT")) {
+							System.err.println("Error Code " + NOT_IMPLEMENTED_CODE + ": " + NOT_IMPLEMENTED_MESSAGE);
+							throw new HTTPException(NOT_IMPLEMENTED_CODE);
+
+						} else {
+							System.err.println("Error Code " + BAD_REQUEST_CODE + ": " + BAD_REQUEST_MESSAGE);
+							throw new HTTPException(BAD_REQUEST_CODE);
+							// throw new Exception("\"GET\" method only");
+						}
 					}
 					URL url = new URL(uri);
 					host = url.getHost();
 					if (host == null || host.equals("")) {
-						throw new Exception("Unable to get host info");
+						System.err.println("Error Code " + BAD_REQUEST_CODE + ": " + BAD_REQUEST_MESSAGE);
+						throw new HTTPException(BAD_REQUEST_CODE);
+						// throw new Exception("Unable to get host info");
 					}
 					port = url.getPort();
 					protocol = url.getProtocol();
@@ -55,7 +77,10 @@ public class HttpRequest {
 						if (protocol.equals("http")) {
 							port = 80;// default port
 						} else {
-							throw new Exception("Wrong protocol, please check");//http procotol only
+							System.err.println("Error Code " + BAD_REQUEST_CODE + ": " + BAD_REQUEST_MESSAGE);
+							throw new HTTPException(BAD_REQUEST_CODE);
+							// throw new Exception("Wrong protocol, please
+							// check");
 						}
 					}
 					query = url.getQuery();
@@ -67,21 +92,26 @@ public class HttpRequest {
 						version = "HTTP/1.0";
 						System.out.println("HTTP version is changed");
 					}
-					Socket newSocket = new Socket(host, port);//New socket to connect server
+					Socket newSocket = new Socket(host, port);// New socket to
+																// connect
+																// server
 					to = new DataOutputStream(newSocket.getOutputStream());
 					String toWrite = method + " " + path + " " + version + "\r\n" + "Host: " + host + "\r\n"
 							+ "Connection: close\r\n\r\n";
-					to.writeBytes(toWrite);//Send request to server
+					to.writeBytes(toWrite);// Send request to server
 					to.flush();
 					BufferedReader rd = new BufferedReader(new InputStreamReader(newSocket.getInputStream()));
 					String line;
 					StringBuilder response = new StringBuilder();
 					response.append("\r\n");
 					while ((line = rd.readLine()) != null) {
-						response.append(line + "\r\n"); //receive and build response string from server
+						response.append(line + "\r\n"); // receive and build
+														// response string from
+														// server
 					}
 					DataOutputStream toClient = new DataOutputStream(socket.getOutputStream());
-					toClient.writeBytes(response.toString()); //Write response to client
+					toClient.writeBytes(response.toString()); // Write response
+																// to client
 					toClient.flush();
 					toClient.close();
 					to.close();
@@ -94,7 +124,8 @@ public class HttpRequest {
 				}
 			}
 		} catch (Exception e) {
-			System.err.println(e.getMessage());
+			if (e.getMessage() != null)
+				System.err.println(e.getMessage());
 		} finally {
 			try {
 				if (socket != null)
